720f9362ee8cadb1b42f72e68a2718849b268946
Trevor Gross <tmgross@umich.edu>
2023-10-23 00:45:32 -0400

    Correct failures in base64

--- rcrypto-sys/tests/b64.c
+++ rcrypto-sys/tests/b64.c
@@ -1,49 +1,41 @@
 #include "test_framework.h"
 
-#define OUTLEN 100
-
 void b64_runner(RcB64Variant var, char *expected, char *name) {
-    uchar msg[] = "Hello, world!";
-    uchar out1[OUTLEN];
-    uchar out2[OUTLEN];
-    size_t newlen;
-
-    int msglen = strlen((char*)msg);
-    int explen = strlen(expected);
-
-    memset(out1, 0, OUTLEN);
-    memset(out2, 0, OUTLEN);
+    const uchar msg[] = "Hello, world! <<?_?>> x";
+    const int msglen = strlen((char*)msg);
+    const int explen = strlen(expected);
     
-    int outlen = rc_base64_encoded_len(RcB64Original, strlen((char*)msg));
+    uchar out_enc[explen];
+    uchar out_dec[msglen];
+    size_t newlen = 0;
+   
+    const int outlen = rc_base64_encoded_len(var, strlen((char*)msg));
     TEST_ASSERT_EQUAL_INT_MESSAGE(explen, outlen, name);
-    
-    rc_base64_encode_ct(var, msg, msglen, out1, OUTLEN, &newlen);
+
+    rc_base64_encode_ct(var, msg, msglen, out_enc, explen, &newlen);
     TEST_ASSERT_EQUAL_INT_MESSAGE(explen, newlen, name);
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(expected, out1, name);
-    printf("m %s\n", out1);
+    TEST_ASSERT_EQUAL_MEMORY_MESSAGE(expected, out_enc, newlen, name);
 
-    rc_base64_decode_ct(var, out1, outlen, out2, OUTLEN, &newlen);
+    newlen = 0;
+    rc_base64_decode_ct(var, out_enc, outlen, out_dec, msglen, &newlen);
     TEST_ASSERT_EQUAL_INT_MESSAGE(msglen, newlen, name);
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(msg, out2, name);
-
-    memset(out2 + newlen, 0, 20);
-    printf("m %s\n", out2);
+    TEST_ASSERT_EQUAL_MEMORY_MESSAGE(msg, out_dec, newlen, name);
 }
 
 void test_b64_original() {
-    b64_runner(RcB64Original, "SGVsbG8sIHdvcmxkIQ==", "b64 original");
+    b64_runner(RcB64Original, "SGVsbG8sIHdvcmxkISA8PD9fPz4+IHg=", "b64 original");
 }
 
 void test_b64_original_nopad() {
-    b64_runner(RcB64OriginalUnpadded, "SGVsbG8sIHdvcmxkIQ", "b64 original nopad");
+    b64_runner(RcB64OriginalUnpadded, "SGVsbG8sIHdvcmxkISA8PD9fPz4+IHg", "b64 original nopad");
 }
 
 void test_b64_url() {
-    b64_runner(RcB64Url, "SGVsbG8sIHdvcmxkIQ==", "b64 url");
+    b64_runner(RcB64Url, "SGVsbG8sIHdvcmxkISA8PD9fPz4-IHg=", "b64 url");
 }
 
 void test_b64_url_nopad() {
-    b64_runner(RcB64UrlUnpadded, "SGVsbG8sIHdvcmxkIQ", "b64 url nopad");
+    b64_runner(RcB64UrlUnpadded, "SGVsbG8sIHdvcmxkISA8PD9fPz4-IHg", "b64 url nopad");
 }
 
 int main() {
